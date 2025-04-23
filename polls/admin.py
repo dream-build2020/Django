6e1194353e0ce49b1c2df49471f446c8564a93f4
kindtom123@gmail.com <kindtom123@gmail.com>
2025-04-24 00:10:34 +0800

    Django 项目

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from polls.models import Job
 
 # Register your models here.
+
+class JobAdmin(admin.ModelAdmin):
+    exclude = ('creator', 'created_date', 'modified_date')
+    list_display = ('job_type', 'job_name', 'job_city')
+
+    def save_model(self, request, obj, form, change):
+        obj.creator = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Job, JobAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import datetime
 # Create your models here.
+
+JobTypes = [
+    (0, "技术类"),
+    (1, "产品类")
+]
+
+Cities = [
+    (0, "北京"),
+    (1, "深圳"),
+    (2, "上海")
+]
+
+class Job(models.Model):
+    job_type = models.SmallIntegerField(blank=False, choices=JobTypes , verbose_name='Job type')
+    job_name = models.CharField(max_length=255, blank=False, verbose_name='Job name')
+    job_city = models.SmallIntegerField(blank=False, choices=Cities , verbose_name='Job city')
+    job_responsibility = models.TextField(blank=False, max_length=1024, verbose_name='Job responsibility')
+    job_requirements = models.TextField(blank=False, max_length=1024, verbose_name='Job requirements')
+    creator = models.ForeignKey(User, verbose_name='Creator',null=True, on_delete=models.SET_NULL)
+    created_date = models.DateTimeField(verbose_name='Created date', default=datetime.now)
+    modified_date = models.DateTimeField(verbose_name='Modified date', default=datetime.now)
